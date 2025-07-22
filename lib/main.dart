de79ce72979b3c7e37f5b8efb29182bb5d7f433e
missmbuvi04 <m.mbuvi@alustudent.com>
2025-07-22 22:59:18 +0300

    updating the firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:juru_stay/app.dart';
-import 'package:juru_stay/features/places/presentation/places_page.dart';
-import 'firebase_options.dart'; // You’ll generate this below
+import 'core/router.dart';
+import 'core/themes.dart';
+import 'firebase_options.dart'; // If you're using FlutterFire CLI
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const App());
+
+  runApp(const ProviderScope(child: App()));
+}
+
+class App extends ConsumerWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      title: 'Juru Stay',
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      routerConfig: router,
+    );
+  }
 }
