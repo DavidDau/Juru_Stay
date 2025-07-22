14797c095208445883c3605fbc2719926cac17c2
missmbuvi04 <m.mbuvi@alustudent.com>
2025-07-22 22:23:10 +0300

    connecting to the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:juru_stay/app.dart';
+import 'package:juru_stay/features/places/presentation/places_page.dart';
+import 'firebase_options.dart'; // You’ll generate this below
 
-void main() {
-  runApp(const ProviderScope(child: App()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const App());
 }
