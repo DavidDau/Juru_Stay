0a3823f628cc9baa5e7ac4d12a8b5f9926145bb4
missmbuvi04 <m.mbuvi@alustudent.com>
2025-07-31 14:12:14 +0300

    updating apps theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,18 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:juru_stay/app.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-
-  
-//   runApp(
-//     const ProviderScope(
-//       child: App(),
-//     ),
-//   );
-// }
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:juru_stay/app.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+  
   runApp(
     const ProviderScope(
       child: App(),
     ),
   );
 }
+
+
